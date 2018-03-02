eaefbd338a540a23cafb67352230d0268090d131
Ann Magdalen Hegge <annmh@hyperx.local>
2018-03-02 12:35:59 +0100

    add navigation to hero detail page
    Signed-off-by: Ann Magdalen Hegge <annmh@hyperx.local>

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,6 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];  
 
   constructor(private heroService: HeroService) { }
 
@@ -18,13 +19,5 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
-  }
-
-  heroes: Hero[];
-
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
